4a6c68892dff1ea73116eb1967fee87e85490aaa
Osman Nuri Okumus <onokumus@gmail.com>
2017-02-02 10:46:25 +0300

    v0.0.2

--- index.js
+++ index.js
@@ -31,11 +31,11 @@ if (process.argv.length > 2) {
 
 fs.readFile(inputFile, 'utf8', function(err, data) {
     if (!err) {
-        var yazi = data.toString().split('\n');
+        var rows = data.toString().split('\n');
 
-        for (var i = 0; i < yazi.length - 1; i++) {
-            var satir = yazi[i].toString().split('\t');
-            result += `${i + 1}\n` + sec(satir[0]) + ' ' + '-->' + ' ' + sec(satir[1]) + '\n' + satir[2] + '\n\n';
+        for (var i = 0; i < rows.length - 1; i++) {
+            var row = rows[i].toString().split('\t');
+            result += `${i + 1}\n` + sec(row[0]) + ' ' + '-->' + ' ' + sec(row[1]) + '\n' + row[2] + '\n\n';
         }
 
         fs.writeFile(outputFile, result, function(err, data) {
